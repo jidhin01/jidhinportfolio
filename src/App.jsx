e991dba96b0b96784121e9cdc1a47e90c1fe711e
TEN <PRO@TEN.local>
2025-05-17 02:17:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {BackgroundBeamsDemo} from"./Herosection"
+import { FloatingNavDemo } from './Fnav'
+
+function App() {
+
+  return (
+    <>
+      <FloatingNavDemo />
+      <BackgroundBeamsDemo />
+     
+    </>
+  )
+}
+
+export default App
